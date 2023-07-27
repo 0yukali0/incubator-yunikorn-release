0256c54fff489c1556757b34feb1647581ce9030
0yukali0 <a08h0283@gmail.com>
2023-07-27 08:58:19 +0800

    setting of FALB and Yunikorn

---
+++ helm-charts/yunikorn/cordon.sh
@@ -0,0 +1,3 @@
+ kubectl cordon blade04-k205-lab blade06-k205-lab blade07-k205-lab blade11-k205-lab blade12-k205-lab hp-server-01 hp-server-02    hp-server-03 hp-server-04 master
+# kubectl uncordon blade04-k205-lab blade06-k205-lab blade07-k205-lab blade11-k205-lab blade12-k205-lab hp-server-01 hp-server-02    hp-server-03 hp-server-04 master
+
